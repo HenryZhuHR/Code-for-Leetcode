5b521c8474ae5f54b20768d7a175d7f38aa92f1f
Henry Zhu <38650110+HenryZhuHR@users.noreply.github.com>
2022-03-10 17:43:03 +0800

    [125] 验证回文串

---
+++ 125.验证回文串.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=125 lang=cpp
+ *
+ * [125] 验证回文串
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+// @lc code=start
+class Solution
+{
+  public:
+    bool isPalindrome(string s)
+    {
+        string sgood;
+        for (char ch: s) {
+            if (isalnum(ch)) {
+                sgood += tolower(ch);
+            }
+        }
+        int n = sgood.size();
+        int left = 0, right = n - 1;
+        while (left < right) {
+           if (sgood[left] != sgood[right]) {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+        return true;
+    }
+};
+// @lc code=end

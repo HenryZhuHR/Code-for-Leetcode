1872c00ac81e766cb6646ac98492493bc8f6ef25
HenryZhuHR <296506195@qq.com>
2023-08-04 13:03:58 +0800

    🎒 dynamic-programming/509.斐波那契数.cpp

---
+++ dynamic-programming/509.斐波那契数.cpp
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=cpp
+ *
+ * [509] 斐波那契数
+ *
+ * https://leetcode.cn/problems/fibonacci-number/description/
+ *
+ * algorithms
+ * Easy (66.13%)
+ * Likes:    673
+ * Dislikes: 0
+ * Total Accepted:    576.6K
+ * Total Submissions: 871.9K
+ * Testcase Example:  '2'
+ *
+ * 斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ *
+ *
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ *
+ *
+ * 给定 n ，请计算 F(n) 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：n = 3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：n = 4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0 <= n <= 30
+ *
+ *
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+// @lc code=start
+class Solution
+{
+  public:
+    int fib(int n)
+    {
+        if (n == 0 || n == 1)
+            return n;
+        else
+        {
+            int fib_n_2 = 0;
+            int fib_n_1 = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                int  fib_n   = fib_n_2 + fib_n_1;
+                fib_n_2 = fib_n_1;
+                fib_n_1 = fib_n;
+            }
+            return fib_n_1;
+        }
+    }
+};
+// @lc code=end

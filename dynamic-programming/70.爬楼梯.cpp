d23d02cd984f35fcab8736393fcf79d8678f7ab7
HenryZhuHR <296506195@qq.com>
2023-08-07 17:58:06 +0800

    🎒 dynamic-programming/70.爬楼梯.cpp

--- dynamic-programming/70.爬楼梯.cpp
+++ dynamic-programming/70.爬楼梯.cpp
@@ -60,20 +60,51 @@ class Solution
   public:
     int climbStairs(int n)
     {
-        if (n <= 1)
-            return n;
-
-        int dp_0 = 1; // 爬到1层
-        int dp_1 = 2; // 爬到2层
-        for (int i = 3; i <= n; i++)
+        string solution = "DP_01bag";
+        if (solution == "DP")
         {
-            // 每次只能 1 / 2 ，所以只能倒数第2 /1 前进
-            // 也就是说，取 n-2 n-1
-            int dp_n = dp_0 + dp_1;
-            dp_0     = dp_1;
-            dp_1     = dp_n;
+            if (n <= 1)
+                return n;
+
+            int dp_0 = 1; // 爬到1层
+            int dp_1 = 2; // 爬到2层
+            for (int i = 3; i <= n; i++)
+            {
+                // 每次只能 1 / 2 ，所以只能倒数第2 /1 前进
+                // 也就是说，取 n-2 n-1
+                int dp_n = dp_0 + dp_1;
+                dp_0     = dp_1;
+                dp_1     = dp_n;
+            }
+            return dp_1;
         }
-        return dp_1;
+        else if (solution == "DP_01bag")
+        {
+            // 修改为 每次可以爬 最多 m 个台阶，之前的题目是固定 最多两个
+            // 最后总和为 n
+
+            int m = 2; // 可以选的数 的数组  1,2,3,...,m
+            /**
+             * dp[j] 表示 爬到 j 阶 的方法
+             * 排列数，背包 外层for
+             */
+            vector<int> dp(n + 1, 0);
+            dp[0] = 1;                       // 爬0层仅一种方法
+            for (int j = 1; j <= n; j++)     // 排列数 外层 背包
+            {
+                for (int i = 1; i <= m; i++) // 排列数 内层 物品
+                {
+                    if (j - i >= 0)
+                    {
+                        dp[j] = dp[j]        // 0 不取
+                                + dp[j - i]; // 1 选取}
+                    }
+                }
+            }
+            return dp[n];
+        }
+        else
+            return INT_MIN;
     }
 };
 // @lc code=end

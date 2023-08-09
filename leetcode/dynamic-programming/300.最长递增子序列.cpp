72f2e772e8e144e5e0c29021928a5aa3d91c6bab
HenryZhuHR <296506195@qq.com>
2023-08-09 10:13:44 +0800

    📶  leetcode/dynamic-programming/300.最长递增子序列.cpp

---
+++ leetcode/dynamic-programming/300.最长递增子序列.cpp
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=cpp
+ *
+ * [300] 最长递增子序列
+ *
+ * https://leetcode.cn/problems/longest-increasing-subsequence/description/
+ *
+ * algorithms
+ * Medium (54.87%)
+ * Likes:    3316
+ * Dislikes: 0
+ * Total Accepted:    767.8K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[10,9,2,5,3,7,101,18]'
+ *
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ *
+ * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7]
+ * 的子序列。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [10,9,2,5,3,7,101,18]
+ * 输出：4
+ * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [0,1,0,3,2,3]
+ * 输出：4
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [7,7,7,7,7,7,7]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 2500
+ * -10^4 <= nums[i] <= 10^4
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ *
+ * 你能将算法的时间复杂度降低到 O(n log(n)) 吗?
+ *
+ *
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+// @lc code=start
+class Solution
+{
+  public:
+    int lengthOfLIS(vector<int>& nums)
+    {
+        // 1 <= nums.length <= 2500
+        if (nums.size() <= 1)
+            return nums.size();
+        vector<int> lens(nums.size(), 1);
+        int         max_len = 1;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] > nums[j]) // 找到一个递增的
+                {
+                    lens[i] = max(lens[i], lens[j] + 1);
+                }
+            }
+            if (lens[i] > max_len)
+                max_len = lens[i];
+        }
+        return max_len;
+    }
+};
+// @lc code=end
